3cf53185ef72eb519b45340128321daaa3cc47e8
Ahmadjunaidi101105 <ahmadjunaidibeds@gmail.com>
2024-03-25 13:55:32 +0700

    Membuat program PHP untuk menampilkan bilangan prima 1 - 100

---
+++ Pertemuan3/Belajar.php
@@ -0,0 +1,27 @@
+<?php
+// echo "Saya sedang belajar PHP";
+// echo "<br>";
+// // echo "Hello World...";
+// // 
+
+
+// $a = "Saya Sedang belajar PHP";
+// $b = 5;
+// print $a;
+// echo $b;
+// 
+
+// const nama= "Kang Daniel";
+// echo nama; // Kang Daniel
+
+// define("nama", "Kang Daniel");
+// echo nama; // Kang Daniel
+
+// function tambah($a, $b) {
+//     $total = 10 + 5;
+//     return $total;
+//     }
+//     $total = tambah(10, 5);
+//     echo $total;
+    
+    
